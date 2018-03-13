c560e35ee1140fc870c4a13349f07de25a362059
Viktor Zagvoskin <v.zagvoskin@rambler.ru>
2018-03-13 23:04:41 +0300

    13.03  Забыл добавить таблицу с тегами...

--- django_project/main/admin.py
+++ django_project/main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib import admin
 
-from .models import Question, Document
+from .models import Question, Document, Tag
 
 admin.site.register(Question)
 admin.site.register(Document)
+admin.site.register(Tag)
