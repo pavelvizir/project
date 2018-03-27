486c9aebab2584b84ebea7d8a8f475492e149b78
pavel <pavel.vizir@gmail.com>
2018-03-27 23:15:29 +0300

    Started cleaning raw_data_provider.py

--- data_provider/raw_data_provider.py
+++ data_provider/raw_data_provider.py
@@ -1,56 +1,75 @@
 #!/usr/bin/env python
+''' dummy docstring '''
 import json
-import zmq
 import time
 from imaplib import IMAP4_SSL
+
+import zmq
+
 from imap_credentials import imap_password, imap_username
 
 
-def fetch_emails(addr, port, user, pwd,
-                 uid=None, mail_limit=10, commit_limit=5):
-    ''' returns up to "commit_limit" new emails from INBOX per run
-        touches no more than "mail_limit" mails per multiple runs'''
+def fetch_emails(
+        username,
+        password,
+        uid=0,
+        address='imap.gmail.com',
+        port=993,
+        mail_batch_limit=5,    # Если писем много, то качаем пачкой.
+        mail_total_limit=10):  # Временное ограничение на общее кол-во писем.
+    ''' returns up to "mail_batch_limit" new emails from INBOX per run
+        touches no more than "mail_total_limit" mails per multiple runs'''
 
     def fetch_and_parse(uids):
-        ''' fetches and parses up to "commit_limit" new emails '''
+        ''' fetches and parses up to "mail_batch_limit" new emails '''
 
         result = list()
 
         for uid in uids:
             reply, email_data = imap_server.uid('fetch', uid, '(RFC822)')
+
             if reply == 'OK':
                 raw_email = email_data[0][1]
+                print(email_data[0][0])
+                # ...(UID 10 RFC...
+                # get regexp num from here
                 result.append(raw_email)
 
         return result
 
-    imap_server = IMAP4_SSL('imap.gmail.com')
-    imap_server.login(imap_username, imap_password)
-    imap_server.select(mailbox='INBOX', readonly=True)
+    imap_server = IMAP4_SSL(address, port)              # Подключаемся.
+    imap_server.login(username, password)               # Логинимся.
+    imap_server.select(mailbox='INBOX', readonly=True)  # Пока только INBOX.
+    # Readonly, чтобы не сбросить статус "Не прочитано".
 
-    if uid:
-        reply, data = imap_server.uid('search', None, '{}:*'.format(uid))
-    else:
-        reply, data = imap_server.uid('search', None, 'ALL')
-        uid = 0
+    # Если передан UID, то читаем всё, что новее. Иначе читаем все письма.
+    imap_uid_string = '{}:*'.format(uid) if uid else 'ALL'
+
+    # Получаем tuple из статуса ответа и списка uid писем.
+    reply, data = imap_server.uid('search', None, imap_uid_string)
 
     if reply == 'OK':
-        uids_blist = data[0].split()
-        len_uids_blist = len(uids_blist)
+        uids_binary = data[0].split()  # Получаем строку бинарных чисел.
+        len_uids_binary = len(uids_binary)
 
-        if len_uids_blist < 2:
-            if uids_blist[0] > str(uid).encode():
-                return int(uids_blist[0].decode()), False, fetch_and_parse(uids_blist)
+        if len_uids_binary < 2:
+            if uids_binary[0] > str(uid).encode():
+                return int(uids_binary[0].decode()
+                           ), False, fetch_and_parse(uids_binary)
 
             return uid, False, False
-        elif len_uids_blist > commit_limit:
-            if len_uids_blist > mail_limit:
-                        return int(uids_blist[-mail_limit:][:commit_limit][-1].decode()), True, fetch_and_parse(uids_blist[-mail_limit:][:commit_limit])
-            return int(uids_blist[:commit_limit][-1].decode()), True, fetch_and_parse(uids_blist[:commit_limit])
+        elif len_uids_binary > mail_batch_limit:
+            if len_uids_binary > mail_total_limit:
+                return int(uids_binary[-mail_total_limit:][:mail_batch_limit][-1].decode()
+                           ), True, fetch_and_parse(uids_binary[-mail_total_limit:][:mail_batch_limit])
+
+            return int(uids_binary[:mail_batch_limit][-1].decode()
+                       ), True, fetch_and_parse(uids_binary[:mail_batch_limit])
         else:
-            return int(uids_blist[-1].decode()), False, fetch_and_parse(uids_blist)
+            return int(uids_binary[-1].decode()
+                       ), False, fetch_and_parse(uids_binary)
     else:
-        return 'Something wrong'
+        return '{} returned error: {}'.format(address, reply)
 
 
 REQUEST_TIMEOUT = 2500
@@ -73,9 +92,11 @@ phase = 0
 payload = None
 more_mails = None
 last_uid = None
+
 while True:
     if not expect_reply:
         sequence += 1
+
         if phase == 0:
             words = "[Slave]  Ready to server, Master!"
         elif phase == 1:
@@ -84,16 +105,19 @@ while True:
             words = "[Slave]  I need the UID of last mail I checked, Master!"
         elif phase == 3:
             payload = None
+
             if not more_mails:
                 last_uid = last_uid or r[4]
-                p = fetch_emails('imap.gmail.com', 993, imap_username, imap_password, last_uid)
+                p = fetch_emails(imap_username, imap_password, last_uid)
             else:
                 p = more_mails
-            if p[2] == False:
+
+            if not p[2]:
                 words = "[Slave]  I've checked emails. Nothing new, Master!"
             else:
                 words = "[Slave]  I've checked emails. Here is a new mail, Master!"
                 payload = p[2][0].decode()
+
                 if len(p[2][1:]) > 0:
                     more_mails = (p[0], p[1], p[2][1:])
                 else:
@@ -103,35 +127,44 @@ while True:
         else:
             phase = 0
             time.sleep(10)
+
             continue
         request = json.dumps([sequence, "Slave", phase, words, payload])
         client.send_json(request)
         print(words)
+
         if phase == 3 and not more_mails and not more_new_mails_flag:
             print("[Slave]  Sleeping for 10 seconds.")
             time.sleep(10)
 
     expect_reply = True
+
     while expect_reply:
         socks = dict(poll.poll(REQUEST_TIMEOUT))
+
         if socks.get(client) == zmq.POLLIN:
             reply = client.recv_json()
+
             if not reply:
                 break
             r = json.loads(reply)
+
             if int(r[0]) == sequence:
                 print(r[3])
                 retries_left = REQUEST_RETRIES
+
                 if phase < 3:
-                    phase = r[2]+1
+                    phase = r[2] + 1
                 expect_reply = False
             else:
                 print("[Slave]  Malformed reply from server: %s" % reply)
 
         else:
             print("[Slave]  No response from server, retrying...")
+
             if retries_left > 0:
                 retries_left -= 1
+
                 break
             # Socket is confused. Close and remove it.
             client.setsockopt(zmq.LINGER, 0)
