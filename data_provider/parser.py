89d6bf3c458ebefd999ed3b218dc3912fc4142ea
pavel <pavel.vizir@gmail.com>
2018-04-05 22:38:31 +0300

    Almost done. /api working.

--- data_provider/parser.py
+++ data_provider/parser.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 ''' docstring '''
-from random import randint
-import time
+# from random import randint
+# import time
 import zmq
 import json
 import msgpack
@@ -10,6 +10,7 @@ from email.policy import default
 from email.parser import BytesParser
 import requests
 
+URL = 'http://127.0.0.1:5000/api'
 
 def parse_email(raw_emails):
     ''' parse email '''
@@ -17,8 +18,8 @@ def parse_email(raw_emails):
     for uid, length, raw_email in raw_emails:
         email_dict = dict()
         email = BytesParser(policy=default).parsebytes(raw_email)
-        # email_dict['Date'] = datetime.strptime(
-        #     email['Date'], '%a, %d %b %Y %H:%M:%S %z')
+        email_dict['Date'] = datetime.strptime(
+            email['Date'], '%a, %d %b %Y %H:%M:%S %z')
         email_dict['metadata'] = dict()
         for header in ['From', 'To', 'Delivered-To',
                        'Message-ID', 'Subject']:
@@ -53,48 +54,51 @@ def zmq_master():
         request = msgpack.unpackb(request)
         payload = None
         if request[0] == 0:
-            names = False
-            url = 'http://127.0.0.1:5000/names'
+            get_error = False
             requests_timeout = (5, 30)
             try:
-                names = requests.get(
-                    url,
-                    timeout=requests_timeout)# ,
-                names.raise_for_status()
+                requests_get = requests.get(
+                    URL,
+                    timeout=requests_timeout)
+                requests_get.raise_for_status()
             except requests.exceptions.HTTPError as requests_exception:
+                get_error = True
                 print('HTTP error: {}'.format(requests_exception))
             except requests.exceptions.RequestException as requests_exception:
+                get_error = True
                 print('Connection eror: {}'.format(requests_exception))
 
-            if names:
-                payload = names.json()[0]
-            else:
+            if get_error:
+                print('\n\tProviding UID locally\n')
                 payload = 0
+            else:
+                payload = requests_get.json()[0]
         elif request[0] == 1:
             if request[1]:
+                print('\n\tNew mails arrived.\n')
                 new_emails = parse_email(request[1])
-                for new_email in new_emails:
-                    print("\n\tI've got the email with the following subject:\n\n\t\t{}\n".format(new_email['metadata']["Subject"].upper()))
-                    if new_email['attachments']:
-                        print('\t\tAttachments:\n')
-                        for h in new_email['attachments']:
-                            print('\t\t\t[Filename]: {:<30} [Size]: {:<10} [MIME]: {:<8}\n'.format(h['filename'], len(h['body']), h['MIME']))
-                names = False
-                url = 'http://127.0.0.1:5000/emails'
+                post_error = False
                 requests_timeout = (5, 30)
                 try:
-                    names = requests.post(
-                        url,
+                    requests_post = requests.post(
+                        URL,
                         timeout=requests_timeout,
-                        # data=json.dumps(new_emails[0], default=str))# ,
-                        data=json.dumps(new_emails, default=str))# ,
-                        # json=new_emails[0])# ,
-                    names.raise_for_status()
+                        data=json.dumps(new_emails, default=str))
+                    requests_post.raise_for_status()
                 except requests.exceptions.HTTPError as requests_exception:
+                    post_error = True
                     print('HTTP error: {}'.format(requests_exception))
                 except requests.exceptions.RequestException as requests_exception:
+                    post_error = True
                     print('Connection eror: {}'.format(requests_exception))
-
+                if post_error:
+                    for new_email in new_emails:
+                        print("\n\tI've got the email with the following subject:\n\n\t\t{}\n".format(new_email['metadata']["Subject"].upper()))
+                        if new_email['attachments']:
+                            print('\t\tAttachments:\n')
+                            for h in new_email['attachments']:
+                                print('\t\t\t[Filename]: {:<30} [Size]: {:<10} [MIME]: {:<8}\n'.format(h['filename'], len(h['body']), h['MIME']))
+ 
             else:
                 print('\n\tNo new emails.\n')
         reply = msgpack.packb([request[0], payload])
--- data_provider/server.py
+++ data_provider/server.py
@@ -8,26 +8,21 @@ from flask import Flask, request, make_response
 app = Flask(__name__)
 
 
-@app.route('/names')
-def show_names():
-    '''
-    '''
-
-    return json.dumps([0, ])
-
-
-@app.route('/emails', methods=['POST'])
+@app.route('/api', methods=['GET', 'POST'])
 def show_emails():
-    new_emails = request.get_json(force=True)
-    for new_email in new_emails:
-        print("\n\tI've got the email with the following subject:\n\n\t\t{}\n".format(new_email['metadata']["Subject"].upper()))
-        if new_email['attachments']:
-            print('\t\tAttachments:\n')
-            for h in new_email['attachments']:
-                print('\t\t\t[Filename]: {:<30} [Size]: {:<10} [MIME]: {:<8}\n'.format(h['filename'], len(h['body']), h['MIME']))
-    resp = make_response('preved', 200)
-    resp.headers['X-Something'] = 'A value'
-    return resp
+    if request.method == 'GET':
+        return json.dumps([0, ])
+    else:
+        new_emails = request.get_json(force=True)
+        for new_email in new_emails:
+            print("\n\tI've got the email with the following subject:\n\n\t\t{}\n".format(new_email['metadata']["Subject"].upper()))
+            if new_email['attachments']:
+                print('\t\tAttachments:\n')
+                for h in new_email['attachments']:
+                    print('\t\t\t[Filename]: {:<30} [Size]: {:<10} [MIME]: {:<8}\n'.format(h['filename'], len(h['body']), h['MIME']))
+        resp = make_response('preved', 200)
+        resp.headers['X-Something'] = 'A value'
+        return resp
 
 
 if __name__ == '__main__':
